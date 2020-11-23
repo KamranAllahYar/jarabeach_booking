3f1562c14a57712164066ed4e32884286411fb08
Dorathy <dorathymaryok@gmail.com>
2020-11-23 17:13:57 +0100

    availabilty contd

--- pages/profile.vue
+++ pages/profile.vue
@@ -9,17 +9,17 @@
                     <form action="" class="w-2/3 m-auto space-y-10 h-3/4">
                         <div class="space-y-10" v-if="!signedIn">
                             <div class="flex space-x-24 full">
-                                <input type="text" placeholder="Email Address" class="w-1/2 rounded">
-                                <input type="text" placeholder="Phone Number" class="w-1/2 rounded">
+                                <input type="text" placeholder="Email Address" class="w-1/2 rounded" v-model="guest.email">
+                                <input type="text" placeholder="Phone Number" class="w-1/2 rounded" v-model="guest.tel">
                             </div>
                             <div class="flex w-full space-x-24">
-                                <input type="text" placeholder="First Name" class="w-1/2 rounded">
-                                <input type="text" placeholder="Last Name" class="w-1/2 rounded">
+                                <input type="text" placeholder="First Name" class="w-1/2 rounded" v-model="guest.first_name">
+                                <input type="text" placeholder="Last Name" class="w-1/2 rounded" v-model="guest.last_name">
                             </div>
-                            <input type="text" placeholder="State any dietary concerns (e.g vegetarian)" class="w-full rounded">
+                            <input type="text" placeholder="State any dietary concerns (e.g vegetarian)" v-model="dietary_concerns" class="w-full rounded">
                             <div class="flex w-full space-x-24">
                                 <div class="flex items-center w-1/2 pr-3 border border-gray-500 rounded">
-                                    <input type="text" placeholder="Upload Identification" class="w-full border-none rounded">
+                                    <input type="text" placeholder="Upload Identification" v-model="identification" class="w-full border-none rounded">
                                     <svg 
                                         xmlns="http://www.w3.org/2000/svg" 
                                         width="18" height="18" fill="none" viewBox="0 0 24 24"
@@ -29,8 +29,8 @@
                                         <path d="M21 15v4a2 2 0 01-2 2H5a2 2 0 01-2-2v-4M17 8l-5-5-5 5M12 3v12"/>
                                     </svg>
                                 </div>
-                                <select name="social" id="social_link" class="w-1/2 rounded">
-                                    <option value="info">How did you hear about us</option>
+                                <select name="social" id="social_link" class="w-1/2 rounded" v-model="social_link">
+                                    <option selected value="info">How did you hear about us</option>
                                     <option value="friend">Friend</option>
                                     <option value="employee">Employee</option>
                                     <option value="social-media">Social media</option>
@@ -54,6 +54,7 @@
                         <textarea name="group-name" id="group_name" rows="6" placeholder="Full names of everyone in your group" class="w-full rounded"></textarea>
                     </form>
                     <button class="p-3 mt-10 bg-gray-200 rounded" @click="toggleSignin">Sign-in check</button>
+                    <nuxt-link tag="button" to="/policies" class="p-3 mt-10 bg-gray-200 rounded cursor-pointer ">Next</nuxt-link>
                 </div>
             </div>
         </div>
@@ -65,6 +66,17 @@ export default {
     data() {
         return {
             signedIn: false,
+            guest: [
+                {
+                    email: '',
+                    tel: '',
+                    first_name: '',
+                    last_name: '',
+                }
+            ],
+            dietary_concerns: '',
+            identification: '',
+            social_link: '',
         }
     },
     methods: {
