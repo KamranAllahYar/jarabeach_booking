75acc7756355c55c269c1fe63b177b9d3c6584d2
Dorathy <dorathymaryok@gmail.com>
2020-11-24 11:21:35 +0100

    getting rooms from api

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,32 +1,32 @@
 export default {
-  ssr: false,
-  target: 'static',
-  head: {
-    title: 'Jara Beach Resort',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Jara Beach Resort' }
+    ssr: false,
+    target: 'static',
+    head: {
+        title: 'Jara Beach Resort',
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: 'Jara Beach Resort' }
+        ],
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        ]
+    },
+    css: [],
+    plugins: [],
+    components: true,
+    buildModules: [
+        '@nuxt/typescript-build',
+        '@nuxtjs/tailwindcss',
+        '@nuxtjs/date-fns',
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
-  css: [
-  ],
-  plugins: [
-  ],
-  components: true,
-  buildModules: [
-    '@nuxt/typescript-build',
-    '@nuxtjs/tailwindcss',
-		'@nuxtjs/date-fns',
-  ],
-  modules: [
-    '@nuxtjs/axios',
-    '@nuxtjs/pwa',
-    '@nuxtjs/toast',
-  ],
-  axios: {},
-  build: {}
+    modules: [
+        '@nuxtjs/axios',
+        '@nuxtjs/pwa',
+        '@nuxtjs/toast',
+    ],
+    axios: {
+        baseURL: "http://localhost:8000/api"
+    },
+    build: {}
 }
--- pages/availability.vue
+++ pages/availability.vue
@@ -1,61 +1,85 @@
 <template>
   <div class="h-screen m-10">
-        <div class="flex h-screen w-100">
-            <div class="w-1/6 text-center">
-                <SideBar/>
-            </div>
-            <div class="w-5/6 p-16 border">
-                <div class="py-10 text-3xl font-bold text-center"> Rooms Setup</div>
-                <div class="flex items-center">
-                    <label for="days" class="w-2/6">How many days are you booking for</label>
-                    <select name="days" id="days" v-model="noOfDays" class="text-sm">
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                        <option value="5">5</option>
-                    </select>
-                </div>
-                <div class="my-10 font-medium">Select Room-Type available for specific date</div>
-                <div class="flex items-center p-3 space-x-10 bg-gray-100" v-for="(room, index) in rooms" :key="index">
-                    <div class="flex items-center w-1/2 space-x-5">
-                        <label for="date" class="">Dates:</label>
-                        <input type="date"  id="date" v-model="room.date">
-                    </div>
-                    <div class="flex items-center w-1/2 space-x-5">
-                        <label for="type" class="">Room-type Available:</label>
-                        <select name="type" id="type" v-model="room.types" class="text-sm" >
-                            <option value="">Select Room</option>
-                            <option v-for="type in rooms[0].types" :key="type.id">{{ type.name }}</option>
-                        </select>
-                        
-                    </div>
-                    <div class="rounded hover:bg-white" @click="removeRoom(index)">
-                        <svg 
-                            class="w-6 h-6" fill="none" 
-                            stroke="currentColor" viewBox="0 0 24 24" 
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 12H4"></path>
-                        </svg>
-                    </div>
-                    
-                </div>
-                <div class="flex items-center justify-end py-2 space-x-3" >
-                    <div class="bg-gray-100 rounded" @click="addRoom">  
-                        <svg 
-                            class="w-6 h-6 cursor-pointer hover:text-gray-600" 
-                            fill="none" stroke="currentColor" 
-                            viewBox="0 0 24 24" 
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
-                        </svg>
-                    </div>
-                    
-                </div>
-                
-            </div>
+    <div class="flex h-screen w-100">
+      <div class="w-1/6 text-center">
+        <SideBar />
+      </div>
+      <div class="w-5/6 p-16 border">
+        <div class="py-10 text-3xl font-bold text-center">Rooms Setup</div>
+        <div class="flex items-center">
+          <label for="days" class="w-2/6"
+            >How many days are you booking for</label
+          >
+          <select name="days" id="days" v-model="noOfDays" class="text-sm">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </select>
         </div>
+        <div class="my-10 font-medium">
+          Select Room-Type available for specific date
+        </div>
+        <div
+          class="flex items-center p-3 space-x-10 bg-gray-100"
+          v-for="(room, index) in rooms"
+          :key="index"
+        >
+          <div class="flex items-center w-1/2 space-x-5">
+            <label for="date" class="">Dates:</label>
+            <input type="date" id="date" v-model="room.date" />
+          </div>
+          <div class="flex items-center w-1/2 space-x-5">
+            <label for="type" class="">Room-type Available:</label>
+            <select name="type" id="type" v-model="room.room_id" class="text-sm">
+              <option value="">Select Room</option>
+              <option v-for="type in roomOptions" :key="type.id" :value="type.id">
+                {{ type.name }}
+              </option>
+            </select>
+          </div>
+          <div class="rounded hover:bg-white" @click="removeRoom(index)">
+            <svg
+              class="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M20 12H4"
+              ></path>
+            </svg>
+          </div>
+        </div>
+        <div class="flex items-center justify-end py-2 space-x-3">
+          <div class="bg-gray-100 rounded" @click="addRoom">
+            <svg
+              class="w-6 h-6 cursor-pointer hover:text-gray-600"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+              ></path>
+            </svg>
+          </div>
+        </div>
+        <pre>
+            {{ rooms }}
+        </pre>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
@@ -66,23 +90,17 @@ export default {
             rooms: [
                 {
                     date: null,
-                    types: [
-                        {id:1, name:'Room 1'},
-                        {id:2, name:'Room 2'},
-                        {id:3, name:'Room 3'},
-                        {id:4, name:'Room 4'},
-                        {id:5, name:'Room 5'},
-                        {id:6, name:'Room 6'},
-                    ]
+                    room_id: null,
                 }
             ],
+            roomOptions: []
         }
     },
     methods: {
         addRoom() {
             this.rooms.push({
                 date: null,
-                types: [],
+                room_id: null,
             }); 
         },
         removeRoom(i) {
@@ -92,10 +110,20 @@ export default {
                 this.rooms.splice(i, 1);
             }
         },
+        getRooms(){
+            this.$axios.get("/rooms")
+                .then(res => {
+                    console.log(res.data.data);
+
+                    this.roomOptions = res.data.data
+                })
+        }
+    },
+    mounted(){
+        this.getRooms();
     }
 }
 </script>
 
 <style>
-
 </style>
