e57535381f0514ee962d68c52a300e63542f078a
Raymond Ativie <RaymondAtivie@gmail.com>
2020-11-30 07:59:50 +0100

    add calendars

---
+++ components/RoomCalendar.vue
@@ -0,0 +1,271 @@
+<template>
+    <div class="flex flex-col">
+        <div class="flex items-center mb-6 space-x-4">
+            <button class="px-4 text-white bg-blue-500" @click="prevMonth()">
+                <svg class="w-6 h-6 text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd"></path>
+                </svg>
+            </button>
+            <button class="px-4 text-white bg-blue-500" @click="nextMonth()">
+                <svg class="w-6 h-6 text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path>
+                </svg>
+            </button>
+            <svg v-if="loading" class="w-5 h-5 mr-3 -ml-1 text-black animate-spin" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+            </svg>
+        </div>
+
+        <div class="flex">
+            <div class="w-48">
+                <div class="flex items-center h-10 pr-3 mb-3 text-xl font-bold">
+                    {{ months[calMonth-1] }}
+                    <span class="ml-1 font-light">{{calYear}}</span>
+                </div>
+                <div v-for="roomType in roomTypes" :key="roomType" class="flex items-center w-full h-10 px-3 uppercase border border-gray-900">
+                    {{ roomType }}
+                </div>
+            </div>
+            <div class="flex-1 pb-3 overflow-auto">
+                <div class="flex mb-3">
+                    <div v-for="w in daysInMonth" :key="w" class="flex flex-col items-center justify-center flex-shrink-0 w-10 h-10 text-sm text-center text-black bg-blue-200 border border-gray-900">
+                        <div>{{ w }}</div>
+                        <div class="text-xs uppercase">{{ dayOfWeek(w) }}</div>
+                    </div>
+                </div>
+                <div class="flex" v-for="roomType in roomTypes" :key="roomType">
+                    <div v-for="day in daysInMonth" :key="day"
+                        class="flex items-center justify-center flex-shrink-0 w-10 h-10 text-black border border-gray-900 cursor-pointer"
+                        :class="roomsAvailable(roomType, day) <= 0 ? 'bg-red-200' : 'bg-green-200'"
+                        @click="hoverRoom(roomType, day)">
+                        {{ roomsAvailable(roomType, day) }}
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="flex">
+            <div>
+                <div v-for="h in hoveredRooms" :key="h.room.id"
+                    class="flex items-center mb-2 cursor-pointer"
+                    :class="h.available == true ? 'text-green-600' : 'text-red-600'"
+                    @click="h.available == true ? addToBookedRoom(h.room.id, h.date) : ''">
+
+                    <svg v-if="!isBooked(h.room.id, h.date)" class="inline-block w-6 h-6 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                    </svg>
+                    <svg v-else class="inline-block w-6 h-6 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
+                    </svg>
+
+                    {{ h.room.name }}
+                </div>
+            </div>
+            <div class="ml-12">
+                <div v-for="h in hoveredRooms" :key="h.room.id"
+                    class="text-blue-600 cursor-pointer">
+                    {{ h.room.name }}
+                    <span v-if="h.available == false">- closed</span>
+                    <span v-else-if="h.available == true"></span>
+                    <span v-else>- {{h.available}}</span>
+                </div>
+            </div>
+        </div>
+
+        <!-- <pre>{{ bookedRooms }}</pre> -->
+    </div>
+</template>
+
+<script>
+import getDay from "date-fns/getDay";
+
+export default {
+    data() {
+        return {
+            rooms: [],
+            calMonth: new Date().getMonth() + 1,
+            calYear: new Date().getFullYear(),
+            weekDays: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+            months: [
+                "January",
+                "February",
+                "March",
+                "April",
+                "May",
+                "June",
+                "July",
+                "August",
+                "September",
+                "October",
+                "November",
+                "December",
+            ],
+            availableRooms: [],
+            loading: false,
+            hoveredRooms: [],
+            bookedRooms: [],
+        };
+    },
+    computed: {
+        daysInMonth() {
+            return new Date(this.calYear, this.calMonth, 0).getDate();
+        },
+        dayMonth() {
+            return new Date(this.calYear, this.calMonth, 0);
+        },
+        firstWeekDay() {
+            return getDay(new Date(this.calYear, this.calMonth - 1, 1)) + 1;
+        },
+        roomTypes() {
+            const types = [];
+            this.rooms.forEach((room) => {
+                if (!types.includes(room.type)) {
+                    types.push(room.type);
+                }
+            });
+
+            return types;
+        },
+    },
+    methods: {
+        isBooked(room_id, dateStr) {
+            const ix = this.bookedRooms.findIndex(
+                (br) => room_id == br.room_id && br.date == dateStr
+            );
+
+            return ix >= 0;
+        },
+        addToBookedRoom(room_id, dateStr) {
+            const ix = this.bookedRooms.findIndex(
+                (br) => br.room_id == room_id && br.date == dateStr
+            );
+
+            if (ix < 0) {
+                this.bookedRooms.push({
+                    room_id: room_id,
+                    date: dateStr,
+                });
+            } else {
+                this.bookedRooms.splice(ix, 1);
+            }
+
+            this.$emit("selected", this.bookedRooms);
+        },
+        hoverRoom(roomType, date) {
+            this.hoveredRooms = [];
+            console.log(roomType);
+
+            let m = this.calMonth;
+            if (m < 10) m = `0${m}`;
+
+            let d = date;
+            if (date < 10) d = `0${date}`;
+
+            const dateStr = `${this.calYear}-${m}-${d}`;
+            console.log(dateStr);
+
+            const roomsHere = this.availableRooms[dateStr];
+
+            if (roomsHere) {
+                roomsHere.forEach((room, ix) => {
+                    const rs = this.roomTypeIds(roomType);
+
+                    if (rs.includes(room.room_id)) {
+                        this.hoveredRooms.push({
+                            room: this.getRoom(room.room_id),
+                            available: room.available,
+                            date: dateStr,
+                        });
+                    }
+                });
+            }
+        },
+        getRoom(roomId) {
+            return this.rooms.find((room) => room.id == roomId);
+        },
+        roomsAvailable(roomType, date) {
+            let m = this.calMonth;
+            if (m < 10) m = `0${m}`;
+
+            let d = date;
+            if (date < 10) d = `0${date}`;
+
+            const dateStr = `${this.calYear}-${m}-${d}`;
+
+            const roomsHere = this.availableRooms[dateStr];
+
+            let count = 0;
+
+            if (roomsHere) {
+                roomsHere.forEach((room, ix) => {
+                    const rs = this.roomTypeIds(roomType);
+                    if (rs.includes(room.room_id)) {
+                        if (room.available == true) count++;
+                    }
+                });
+            }
+
+            return count;
+        },
+        roomTypeIds(roomType) {
+            return this.rooms
+                .filter((room) => room.type == roomType)
+                .map((room) => room.id);
+        },
+        dayOfWeek(day) {
+            return this.weekDays[
+                getDay(new Date(this.calYear, this.calMonth - 1, day))
+            ];
+        },
+        nextMonth() {
+            if (this.calMonth == 12) {
+                this.calMonth = 1;
+                this.calYear++;
+            } else {
+                this.calMonth++;
+            }
+
+            this.getRoomsForMonth();
+        },
+        prevMonth() {
+            if (this.calMonth == 1) {
+                this.calMonth = 12;
+                this.calYear--;
+            } else {
+                this.calMonth--;
+            }
+
+            this.getRoomsForMonth();
+        },
+        getRooms() {
+            this.$axios.get("/rooms").then((res) => {
+                this.rooms = res.data.data;
+            });
+        },
+        getRoomsForMonth() {
+            this.loading = true;
+
+            const dataToSend = {
+                date: `${this.calYear}-${this.calMonth}-01`,
+                days: this.daysInMonth,
+            };
+
+            setTimeout(() => {
+                this.$axios
+                    .post("/available-rooms-days", dataToSend)
+                    .then((res) => {
+                        this.availableRooms = res.data.data;
+                    })
+                    .finally(() => {
+                        this.loading = false;
+                    });
+            }, 0);
+        },
+    },
+    mounted() {
+        this.getRooms();
+        this.getRoomsForMonth();
+    },
+};
+</script>
--- pages/availability.vue
+++ pages/availability.vue
@@ -1,86 +1,82 @@
 <template>
-  <div class="h-screen m-10">
-    <div class="flex h-screen w-100">
-      <div class="w-1/6 text-center">
-        <SideBar />
-      </div>
-      <div class="w-5/6 p-16 border">
-        <div class="py-10 text-3xl font-bold text-center">Rooms Setup</div>
-        <div class="flex items-center">
-          <label for="days" class="w-2/6"
-            >How many days are you booking for</label
-          >
-          <select name="days" id="days" v-model="noOfDays" class="text-sm">
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-          </select>
+    <div class="h-screen m-10">
+        <div class="flex h-screen w-100">
+            <div class="w-1/6 text-center">
+                <SideBar />
+            </div>
+            <div class="w-5/6 p-16 border">
+
+                <RoomCalendar class="w-full mb-6" @selected="selectRooms($event)" />
+
+                <!-- <Calendar class="w-1/2" /> -->
+
+                <div class="py-10 text-3xl font-bold text-center">Rooms Setup</div>
+                <div class="flex items-center">
+                    <label for="days" class="w-2/6">How many days are you booking for</label>
+                    <select name="days" id="days" v-model="noOfDays" class="text-sm">
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                    </select>
+                </div>
+                <div class="my-10 font-medium">
+                    Select Room-Type available for specific date
+                </div>
+                <div
+                    class="flex items-center p-3 space-x-10 bg-gray-100"
+                    v-for="(room, index) in rooms"
+                    :key="index">
+                    <div class="flex items-center w-1/2 space-x-5">
+                        <label for="date" class="">Dates:</label>
+                        <input type="date" id="date" v-model="room.date" />
+                    </div>
+                    <div class="flex items-center w-1/2 space-x-5">
+                        <label for="type" class="">Room-type Available:</label>
+                        <select name="type" id="type" v-model="room.room_id" class="text-sm">
+                            <option value="">Select Room</option>
+                            <option v-for="type in roomOptions" :key="type.id" :value="type.id">
+                                {{ type.name }}
+                            </option>
+                        </select>
+                    </div>
+                    <div class="rounded hover:bg-white" @click="removeRoom(index)">
+                        <svg
+                            class="w-6 h-6"
+                            fill="none"
+                            stroke="currentColor"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M20 12H4"></path>
+                        </svg>
+                    </div>
+                </div>
+                <div class="flex items-center justify-end py-2 space-x-3">
+                    <div class="bg-gray-100 rounded" @click="addRoom">
+                        <svg
+                            class="w-6 h-6 cursor-pointer hover:text-gray-600"
+                            fill="none"
+                            stroke="currentColor"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
+                        </svg>
+                    </div>
+                </div>
+                <pre>{{ rooms }}</pre>
+                <span @click="gotoNext()" class="p-3 mt-10 bg-gray-200 rounded cursor-pointer ">Next</span>
+            </div>
         </div>
-        <div class="my-10 font-medium">
-          Select Room-Type available for specific date
-        </div>
-        <div
-          class="flex items-center p-3 space-x-10 bg-gray-100"
-          v-for="(room, index) in rooms"
-          :key="index"
-        >
-          <div class="flex items-center w-1/2 space-x-5">
-            <label for="date" class="">Dates:</label>
-            <input type="date" id="date" v-model="room.date" />
-          </div>
-          <div class="flex items-center w-1/2 space-x-5">
-            <label for="type" class="">Room-type Available:</label>
-            <select name="type" id="type" v-model="room.room_id" class="text-sm">
-              <option value="">Select Room</option>
-              <option v-for="type in roomOptions" :key="type.id" :value="type.id">
-                {{ type.name }}
-              </option>
-            </select>
-          </div>
-          <div class="rounded hover:bg-white" @click="removeRoom(index)">
-            <svg
-              class="w-6 h-6"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M20 12H4"
-              ></path>
-            </svg>
-          </div>
-        </div>
-        <div class="flex items-center justify-end py-2 space-x-3">
-          <div class="bg-gray-100 rounded" @click="addRoom">
-            <svg
-              class="w-6 h-6 cursor-pointer hover:text-gray-600"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-              ></path>
-            </svg>
-          </div>
-        </div>
-        <pre>
-            {{ rooms }}
-        </pre>
-        <span @click="gotoNext()" class="p-3 mt-10 bg-gray-200 rounded cursor-pointer ">Next</span>
-      </div>
     </div>
-  </div>
 </template>
 
 <script>
@@ -92,46 +88,49 @@ export default {
                 {
                     date: null,
                     room_id: null,
-                }
+                },
             ],
-            roomOptions: []
-        }
+            roomOptions: [],
+        };
     },
     methods: {
         addRoom() {
             this.rooms.push({
                 date: null,
                 room_id: null,
-            }); 
+            });
         },
         removeRoom(i) {
-            if(this.rooms.length == 1){
+            if (this.rooms.length == 1) {
                 return;
             } else {
                 this.rooms.splice(i, 1);
             }
         },
-        getRooms(){
-            this.$axios.get("/rooms")
-                .then(res => {
-                    console.log(res.data.data);
+        getRooms() {
+            this.$axios.get("/rooms").then((res) => {
+                console.log(res.data.data);
 
-                    this.roomOptions = res.data.data
-                })
+                this.roomOptions = res.data.data;
+            });
         },
-        gotoNext(){
-          this.$store.commit("UPDATE_ROOMS", {
-            room_id: this.rooms[0].room_id,
-            date: this.rooms[0].date
-          });
+        gotoNext() {
+            this.$store.commit("UPDATE_ROOMS", {
+                room_id: this.rooms[0].room_id,
+                date: this.rooms[0].date,
+            });
 
-          this.$router.push({path: "/profile"})
+            this.$router.push({ path: "/profile" });
+        },
+        selectRooms(rooms){
+          console.log(rooms);
+          this.rooms = rooms;
         }
     },
-    mounted(){
+    mounted() {
         this.getRooms();
-    }
-}
+    },
+};
 </script>
 
 <style>
