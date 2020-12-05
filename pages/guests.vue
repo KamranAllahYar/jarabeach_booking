4f8c6f9d17ac5063ed183a93af384d3e27b913fd
Raymond Ativie <RaymondAtivie@gmail.com>
2020-12-05 04:50:00 +0100

    guest page cleanup

--- pages/guests.vue
+++ pages/guests.vue
@@ -3,115 +3,75 @@
         <div class="text-2xl font-medium text-center">
             What's <b>your guest</b> make-up?
         </div>
-        <div class="max-w-md px-6 py-4 mx-auto mt-10 bg-white border rounded-lg">
+        <div class="max-w-md px-6 py-6 mx-auto mt-10 bg-white border rounded-lg">
             <div class="border rounded-lg">
+
                 <div class="p-4">
-                    <div class="mb-4 font-semibold">Select guest make-up</div>
-                    <div class="w-full">
-                        <div class="flex items-center space-x-4">
-                            <div
-                                class="flex items-center pl-2 space-x-2 rounded-md focus-within:ring">
-                                <div>
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        width="16"
-                                        height="16"
-                                        fill="none"
-                                        viewBox="0 0 24 24"
-                                        stroke="currentColor"
-                                        stroke-width="1"
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        class="text-brand-blue-400">
-                                        <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2" />
-                                        <circle cx="12" cy="7" r="4" />
-                                    </svg>
-                                </div>
-                                <div class="font-light">Adults</div>
-                                <select
-                                    name="adult"
-                                    id="adult"
-                                    v-model="noOfAdult"
-                                    class="text-xs border-0 rounded-md outline-none focus:outline-none"
-                                    style="box-shadow: none">
-                                    <option
-                                        v-for="num in 10"
-                                        :value="num"
-                                        :key="num">
-                                        {{ num }}
-                                    </option>
-                                </select>
-                            </div>
+                    <div class="mb-4 font-semibold">Select Guest make-up</div>
 
-                            <div class="flex items-center space-x-2">
-                                <div>
-                                    <svg
-                                        class="w-5 h-5 text-brand-blue-400"
-                                        fill="none"
-                                        stroke="currentColor"
-                                        viewBox="0 0 24 24"
-                                        xmlns="http://www.w3.org/2000/svg">
-                                        <path
-                                            stroke-linecap="round"
-                                            stroke-linejoin="round"
-                                            stroke-width="1"
-                                            d="M9.172 16.172a4 4 0 015.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-                                    </svg>
-                                </div>
-                                <div class="font-light">Children</div>
-                                <select
-                                    name="adult"
-                                    id="children"
-                                    v-model="noOfChildren"
-                                    class="text-xs border-0">
-                                    <option
-                                        v-for="num in 5"
-                                        :value="num"
-                                        :key="num">
-                                        {{ num }}
-                                    </option>
-                                </select>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="p-4 border-t" v-if="noOfChildren">
-                    <div class="font-semibold">
-                        Specify children's age (years)
-                    </div>
-                    <div class="grid w-full grid-cols-3 gap-y-3">
-                        <div class="flex items-center space-x-2" v-for="age in noOfChildren" :key="age">
-                            <div>
-                                <svg
-                                    class="w-5 h-5 text-brand-blue-400"
-                                    fill="none"
-                                    stroke="currentColor"
-                                    viewBox="0 0 24 24"
-                                    xmlns="http://www.w3.org/2000/svg">
-                                    <path
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        stroke-width="1"
-                                        d="M9.172 16.172a4 4 0 015.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-                                </svg>
-                            </div>
-
-                            <select
-                                name="adult"
-                                id="children"
-                                v-model="childAge"
-                                class="text-xs border-0">
+                    <div class="flex items-center space-x-6">
+                        <div class="flex items-center pl-2 space-x-2 rounded-md focus-within:ring">
+                            <svg class="w-5 h-5" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M13.422 14.531a6.552 6.552 0 00-11.845 0M7.5 3.281a3.282 3.282 0 01-3.281 3.281 3.281 3.281 0 106.562 0A3.281 3.281 0 017.5 3.283v0z" stroke="#225A89" stroke-width=".8" stroke-linecap="round" stroke-linejoin="round" />
+                                <path d="M11.148 11.892a9.11 9.11 0 002.12-.63.938.938 0 00.476-1.225l-.86-2.006a2.813 2.813 0 01-.228-1.109V5.626a5.156 5.156 0 10-10.312 0v1.298c0 .38-.077.757-.228 1.108l-.86 2.006a.937.937 0 00.477 1.225 9.11 9.11 0 002.12.63" stroke="#225A89" stroke-width=".8" stroke-linecap="round" stroke-linejoin="round" />
+                            </svg>
+                            <!-- <div class="font-light">Adults</div> -->
+                            <select name="adult" id="adult" v-model="noOfAdult"
+                                class="border-0 rounded-md outline-none focus:outline-none"
+                                style="box-shadow: none">
+                                <option value="0">Adults</option>
                                 <option
-                                    v-for="num in 14"
+                                    v-for="num in 10"
                                     :value="num"
                                     :key="num">
                                     {{ num }}
                                 </option>
                             </select>
                         </div>
+
+                        <div class="flex items-center pl-2 space-x-2 rounded-md focus-within:ring">
+                            <svg class="w-5 h-5" viewBox="0 0 16 16" stroke="currentColor" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M13.75 6.583h-.01C13.5 3.55 10.706 1.55 8 1.55s-5.5 2-5.74 5.033h-.01a1.75 1.75 0 100 3.5h.01a6 6 0 0011.48 0h.01a1.75 1.75 0 100-3.5z" stroke="#225A89" stroke-width=".8" stroke-linecap="round" stroke-linejoin="round" />
+                                <path d="M8 1.55a1.5 1.5 0 01.596 2.876 1.5 1.5 0 01-2.01-.876M9.323 11.334a2 2 0 01-2.646 0M11.5 7.333a1 1 0 01-2 0M6.5 7.333a1 1 0 01-2 0" stroke="#225A89" stroke-width=".8" stroke-linecap="round" stroke-linejoin="round" />
+                            </svg>
+
+                            <select v-model="noOfChildren"
+                                class="border-0 rounded-md outline-none focus:outline-none"
+                                style="box-shadow: none">
+                                <option value="0">Children</option>
+                                <option v-for="num in 5" :value="num" :key="num">
+                                    {{ num }}
+                                </option>
+                            </select>
+                        </div>
                     </div>
                 </div>
+
+                <div class="p-4 border-t" v-if="noOfChildren">
+                    <div class="mb-4 font-semibold">
+                        Specify children's age (years)
+                    </div>
+                    <div class="grid w-full grid-cols-3 gap-x-3 gap-y-3">
+                        <div class="flex items-center pl-2 rounded-md focus-within:ring" v-for="age in noOfChildren" :key="age">
+                            <svg class="w-5 h-5" viewBox="0 0 16 16" stroke="currentColor" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M13.75 6.583h-.01C13.5 3.55 10.706 1.55 8 1.55s-5.5 2-5.74 5.033h-.01a1.75 1.75 0 100 3.5h.01a6 6 0 0011.48 0h.01a1.75 1.75 0 100-3.5z" stroke="#225A89" stroke-width=".8" stroke-linecap="round" stroke-linejoin="round" />
+                                <path d="M8 1.55a1.5 1.5 0 01.596 2.876 1.5 1.5 0 01-2.01-.876M9.323 11.334a2 2 0 01-2.646 0M11.5 7.333a1 1 0 01-2 0M6.5 7.333a1 1 0 01-2 0" stroke="#225A89" stroke-width=".8" stroke-linecap="round" stroke-linejoin="round" />
+                            </svg>
+
+                            <select class="border-0 rounded-md outline-none focus:outline-none" style="box-shadow: none">
+                                <option v-for="num in 15" :value="num" :key="num">
+                                    {{ num }}
+                                </option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="flex w-full mt-6 space-x-3">
+                <OutlineButton class="flex-1">Back</OutlineButton>
+                <SolidButton class="flex-1">Next</SolidButton>
             </div>
         </div>
 
