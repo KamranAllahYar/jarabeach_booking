f3b009f8df6b43c0b708fec5fa022499eab981be
Raymond Ativie <RaymondAtivie@gmail.com>
2020-12-07 14:52:55 +0100

    remove text-sm

--- components/extras/SpecialLookout.vue
+++ components/extras/SpecialLookout.vue
@@ -5,14 +5,14 @@
         </div>
         <div class="w-7/12 p-6">
             <div class="font-semibold">The Lookout Experience</div>
-            <p class="mt-3 text-sm font-light leading-relaxed text-gray-600">
+            <p class="mt-3 font-light leading-relaxed text-gray-600">
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum, sint impedit possimus itaque facere placeat debitis.
                 Totam nostrum aspernatur enim corporis alias, deserunt et quas aut error maxime ad pariatur.
                 Voluptas accusamus molestias cum aperiam blanditiis. Harum eos, reprehenderit sequi quas suscipit repudiandae sunt blanditiis
                 corrupti ut magni praesentium nemo omnis accusamus ex dolores. Eaque rem non eveniet provident. Non. Eaque rem non eveniet provident. Non.
             </p>
-            <div class="mt-6 text-sm font-semibold">What date would you like to have this</div>
-            <div class="flex items-center mt-3 space-x-5 text-sm font-light ">
+            <div class="mt-6 font-semibold">What date would you like to have this</div>
+            <div class="flex items-center mt-3 space-x-5 font-light ">
                 <div>
                     <input type="radio" name="lookout" id="first_date" class="mr-3 focus-within:ring-0">
                     <label for="first_date">Tues, Nov 9th 2020 </label>
