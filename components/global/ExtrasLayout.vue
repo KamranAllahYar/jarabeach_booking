99f038ac681a3ea74d22cc711a94622cf2cebf96
Raymond Ativie <RaymondAtivie@gmail.com>
2020-12-09 11:21:33 +0100

    spacing

--- components/global/ExtrasLayout.vue
+++ components/global/ExtrasLayout.vue
@@ -2,12 +2,19 @@
     <div>
         <SpecialLookout v-if="currentSpecial == 'lookout'" @next="gotoNext('lookoutMeal')" />
         <SpecialLookoutMeal v-if="currentSpecial == 'lookoutMeal'" @next="gotoNext('massage')" />
+
         <SpecialMassage v-else-if="currentSpecial == 'massage'" @next="gotoNext('quadbikes')" />
+
         <SpecialQuadBikes v-else-if="currentSpecial == 'quadbikes'" @next="gotoNext('photoshoot')" />
+
         <SpecialPhotoshoot v-if="currentSpecial == 'photoshoot'" @next="gotoNext('drinks')" />
+
         <SpecialDrinkCollection v-if="currentSpecial == 'drinks'" @next="gotoNext('cake')" />
+
         <SpecialCake v-else-if="currentSpecial == 'cake'" @next="gotoNext('roomDecoration')" />
+
         <SpecialRoomDecoration v-else-if="currentSpecial == 'roomDecoration'" @next="gotoNext('domesticStaff')" />
+
         <SpecialDomesticStaff v-else-if="currentSpecial == 'domesticStaff'" @next="gotoNext('domesticStaffInfo')" />
         <SpecialDomesticStaffInfo v-else-if="currentSpecial == 'domesticStaffInfo'" />
     </div>
