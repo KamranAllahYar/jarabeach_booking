e590e43cd830590961217ccb463c4a28a95915e4
Raymond Ativie <RaymondAtivie@gmail.com>
2021-01-20 09:25:21 +0100

    remove space

--- components/global/TopProgressBar.vue
+++ components/global/TopProgressBar.vue
@@ -1,22 +1,22 @@
 <template>
     <div class="flex justify-center w-full space-x-0">
         <ProgressNav first to="/guests" :done="$store.state.guests_done">
-            1. <span class="hidden md:inline-block">&nbsp; Guests</span>
+            1. <span class="hidden md:inline-block"> Guests</span>
         </ProgressNav>
         <ProgressNav to="/availability" :done="$store.state.availability_done">
-            2. <span class="hidden md:inline-block">&nbsp; Availability</span>
+            2. <span class="hidden md:inline-block"> Availability</span>
         </ProgressNav>
         <ProgressNav to="/extras" :done="$store.state.availability_done">
-            3. <span class="hidden md:inline-block">&nbsp; Extras</span>
+            3. <span class="hidden md:inline-block"> Extras</span>
         </ProgressNav>
         <ProgressNav to="/profile" :done="$store.state.profile_done">
-            4. <span class="hidden md:inline-block">&nbsp; Profile</span>
+            4. <span class="hidden md:inline-block"> Profile</span>
         </ProgressNav>
         <ProgressNav to="/policies" :done="$store.state.policy_done">
-            5. <span class="hidden md:inline-block">&nbsp; Policies</span>
+            5. <span class="hidden md:inline-block"> Policies</span>
         </ProgressNav>
         <ProgressNav last to="/summary">
-            6. <span class="hidden md:inline-block">&nbsp; Payment</span>
+            6. <span class="hidden md:inline-block"> Payment</span>
         </ProgressNav>
     </div>
 </template>
