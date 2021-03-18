5d3797bba97ab8579d3e7c78ab3d5ef7ae4e6923
Raymond Ativie <RaymondAtivie@gmail.com>
2021-03-18 10:19:02 +0100

    fix booking click issue

--- pages/summary.vue
+++ pages/summary.vue
@@ -75,7 +75,7 @@
                         <template v-if="shouldShowPaymentButton">
                             <div class="relative flex-shrink-0" v-if="shouldShowBookOnHold" @mouseenter="holdDisclaimerToggle(true)" @mouseleave="holdDisclaimerToggle(false)">
                                 <MainButton :loading="loading" @click.native="bookOnHoldBooking()">
-                                    <div class="flex justify-center" v-popover:tooltip="'This is a string value'">
+                                    <div class="flex justify-center">
                                         <svg class="w-6 h-6 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                             <path fill-rule="evenodd" d="M4 4a2 2 0 00-2 2v4a2 2 0 002 2V6h10a2 2 0 00-2-2H4zm2 6a2 2 0 012-2h8a2 2 0 012 2v4a2 2 0 01-2 2H8a2 2 0 01-2-2v-4zm6 4a2 2 0 100-4 2 2 0 000 4z" clip-rule="evenodd"></path>
                                         </svg>
@@ -241,7 +241,8 @@ export default {
             this.loading = false;
         },
         async bookOnHoldBooking() {
-          console.log("Clickling book on hold")
+            console.log("Clickling book on hold");
+
             if (this.loading) return;
 
             this.loading = true;
