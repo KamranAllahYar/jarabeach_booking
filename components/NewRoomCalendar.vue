72a43487afce0df3c26f9ba295abbb1fd1ceabd7
Raymond Ativie <RaymondAtivie@gmail.com>
2021-09-08 01:16:43 +0100

    fix broken UX

--- components/NewRoomCalendar.vue
+++ components/NewRoomCalendar.vue
@@ -109,7 +109,7 @@
                                     <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                                 </svg>
                             </div>
-                            <div class="grid grid-flow-col grid-cols-2 gap-x-4" :class="notAllRooms && seRoom == 'family' ? 'grid-rows-4' : 'grid-rows-5'" v-else>
+                            <div class="grid grid-flow-col gap-x-4" :class="hoveredRooms.length <= 0 ? 'grid-rows-0 grid-cols-1' : notAllRooms && seRoom == 'family' ? 'grid-rows-4 grid-cols-2' : 'grid-rows-5 grid-cols-2'" v-else>
                                 <template v-for="h in hoveredRooms">
                                     <div v-if="h.available == true" :key="h.room.id" class="flex items-center py-2 cursor-pointer"
                                         @click="h.available == true ? addToBookedRoom(h.room, h.date) : ''">
@@ -126,7 +126,7 @@
                                     </div>
                                 </template>
                                 <div class="px-1" v-if="hoveredRooms.length <= 0">
-                                    No rooms available for booking on this date
+                                    No common rooms available for booking on these dates
                                 </div>
                             </div>
                         </div>
