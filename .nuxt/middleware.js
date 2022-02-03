b70ebfc540a60c471b7856c5100d09cb2b687b6a
Raymond Ativie <raymondativie@gmail.com>
2022-02-03 01:10:31 +0100

    remove nuxt

--- .nuxt/middleware.js
+++
@@ -1,6 +0,0 @@
-const middleware = {}
-
-middleware['clear'] = require('../middleware/clear.js')
-middleware['clear'] = middleware['clear'].default || middleware['clear']
-
-export default middleware
