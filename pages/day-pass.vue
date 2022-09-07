ea60d57b0de2379d21db821579d9bce117dcd8cd
David Joseph <Davidjoe781227@gmail.com>
2022-09-07 16:44:39 +0100

    Daypass Booking Integration Logic Started

---
+++ pages/day-pass.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>Day Pass is available {{routeName}}</div>
+</template>
+
+<script>
+export default {
+	layout: 'day-pass',
+    computed: {
+        routeName() {
+            return this.$route.name;
+        }
+    }
+
+}
+</script>
