8b7f042aba098d929c0f912938a0267008e4ee6c
David Joseph <Davidjoe781227@gmail.com>
2022-09-27 09:07:50 +0100

    UI update

--- pages/day-pass-availability.vue
+++ pages/day-pass-availability.vue
@@ -1,27 +1,37 @@
 <template>
 	<div class="max-w-4xl px-4 mx-auto md:px-0">
-		<div class="items-center justify-between mb-4 space-y-2 md:flex md:space-y-0">
+		<div class="flex items-center justify-between mb-4">
 			<div class="font-bold text-md md:text-lg">Are you booking on a weekday (MON-THUR), weekend (FRI-SUN) or Seasonal?</div>
-			<div>More about seasonal <a href="https://www.jarabeachresort.com/day-pass" class="font-bold" target="_blank">here</a></div>
+			<div class="hidden text-sm md:block">More about seasonal <a href="https://www.jarabeachresort.com/day-pass" class="font-bold" target="_blank">here</a></div>
 		</div>
-		<div class="flex flex-col items-start justify-between md:mt-0 md:items-center md:flex-row">
+		<div class="items-center justify-between md:flex md:mt-0">
 			<div class="mb-2 font-semibold md:hidden">Select your option</div>
-			<select name="option_type" id="option_type" v-model="option_type" class="order-last block border rounded-md outline-none md:order-first focus:outline-none" style="box-shadow: none">
+			<select name="option_type" id="option_type" v-model="option_type" class="block border rounded-md outline-none focus:outline-none" style="box-shadow: none">
 				<option value="weekend">Weekend</option>
 				<option value="weekday">Weekday</option>
 				<option value="seasonal">Seasonal</option>
 			</select>
-			<div class="items-center order-first space-y-2 md:space-y-0 md:space-x-4 md:order-last md:flex">
+			<div class="items-center hidden md:flex md:space-x-4" v-if="option_type === 'seasonal'">
 				<div class="font-semibold">Check Seasonal Dates here</div>
-				<select name="option_type" id="option_type" class="block border rounded-md outline-none focus:outline-none" style="box-shadow: none">
-					<option value="weekend" v-for="(date, index) in noDiscountDates" :key="index">{{dropdownDate(date)}}</option>
+				<select name="option_type" id="option_type" class="block border rounded-md outline-none focus:outline-none" style="box-shadow: none" @change="selectedDate = 'see your date'" v-model="selectedDate">
+					<option value="see your date" selected>See Seasonal Date</option>
+					<option class="pointer-events-none" v-for="(date, index) in sortedNoDiscountDays" :key="index">{{dropdownDate(date)}}</option>
 				</select>
 			</div>
 		</div>
+		<div class="mt-4 md:hidden" v-if="option_type === 'seasonal'">
+			<div class="font-semibold">Check Seasonal Dates here</div>
+			<select name="option_type" id="option_type" class="block mt-2 border rounded-md outline-none focus:outline-none" style="box-shadow: none" @change="selectedDate = 'see your date'" v-model="selectedDate">
+				<option value="see your date" selected>See Seasonal Date</option>
+				<option class="pointer-events-none" v-for="(date, index) in sortedNoDiscountDays" :key="index">{{dropdownDate(date)}}</option>
+			</select>
+		</div>
+		<div class="mt-4 text-sm md:hidden">More about seasonal <a href="https://www.jarabeachresort.com/day-pass" class="font-bold" target="_blank">here</a></div>
 		<div class="mt-6">
 			<vc-calendar :min-date="new Date().toISOString().split('T')[0]" @dayclick="onDayClick" :attributes="attributes" v-if="option_type === 'seasonal'"/>
 			<vc-calendar :min-date="new Date().toISOString().split('T')[0]" @dayclick="onDayClick" :attributes="attributes" :disabled-dates="disabledDates" v-else/>
 		</div>
+
 		<div class="w-32 mt-6 space-x-3" v-if="canGoToNext">
 			<!-- <MainButton outline>Back</MainButton> -->
 			<MainButton @click="goToNext()">Next</MainButton>
@@ -41,6 +51,7 @@ export default {
 		return {
 			option_type: 'weekend',
 			date: null,
+			selectedDate: 'see your date'
 		};
 	},
 	mounted() {
@@ -126,6 +137,9 @@ export default {
 		...mapGetters({
 			noDiscountDates: 'noDiscountDates',
 		}),
+		sortedNoDiscountDays(){
+			return this.noDiscountDates.sort((a, b) => a - b)
+		},
 		isDateSeasonalDate() {
 			const isDateAvailable = this.noDiscountDates.filter(d => {
 				return moment(this.bookingDate).format('DD/MM/YYYY') === moment(d).format('DD/MM/YYYY');
